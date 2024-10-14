2b9d27e060f6e4959571cf57f27966b32019a095
Iewnfod <shangzhiqiuyue@outlook.com>
2024-10-14 14:50:24 +0800

    feat: main framework

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Box, Grid} from "@mui/joy";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {Toaster} from "react-hot-toast";
+import DashPage from "../pages/DashPage.tsx";
+import SchedulePage from "../pages/SchedulePage.tsx";
+import RankPage from "../pages/RankPage.tsx";
+import SkillsRankPage from "../pages/SkillsRankPage.tsx";
+import MatchPage from "../pages/MatchPage.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const router = createBrowserRouter([
+        {
+            path: '/',
+            element: <DashPage/>
+        },
+        {
+            path: '/schedule',
+            element: <SchedulePage/>
+        },
+        {
+            path: '/rank',
+            element: <RankPage/>
+        },
+        {
+            path: '/srank',
+            element: <SkillsRankPage/>
+        },
+        {
+            path: '/match',
+            element: <MatchPage/>
+        },
+    ]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Box sx={{overflow: 'hidden', userSelect: 'none'}}>
+            <Toaster/>
+            <Grid
+                container
+                spacing={0}
+                direction="column"
+                alignItems="center"
+                justifyContent="center"
+                sx={{position: 'absolute'}}
+                style={{minHeight: '100vh', left: 0, top: 0, width: '100vw'}}
+            >
+                <Grid xs={10}>
+                    <Box sx={{display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center"}}>
+                        <RouterProvider router={router}/>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Box>
+    );
 }
 
-export default App
+export default App;
