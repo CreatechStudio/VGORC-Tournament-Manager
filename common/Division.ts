e907c13381e33c929e20fd22054f466b1cd90bbb
lightumcc <jessiezhu@createchstudio.com>
2024-10-14 18:50:01 +0800

    feat(common): division

---
+++ common/Division.ts
@@ -0,0 +1,29 @@
+export class Division {
+    data: string[] = [];
+
+    constructor() {}
+
+    add(divisionName: string) {
+        if (this.data.includes(divisionName)) {
+            throw "Division already exist";
+        }
+        if (divisionName.length === 0) {
+            throw "Division name should not be empty";
+        }
+        this.data.push(divisionName);
+    }
+
+    get() {
+        return this.data.sort();
+    }
+
+    delete(divisionName: string) {
+        let index = this.data.indexOf(divisionName);
+        if (index > -1) {
+            this.data.splice(index, 1);
+        } else {
+            throw "Division does not exist";
+        }
+    }
+
+}
