ade6e3d3c6371d51be1a6c9f71791ed0be471939
lightumcc <jessiezhu@createchstudio.com>
2024-10-14 18:50:08 +0800

    feat(common): team

---
+++ common/Team.ts
@@ -0,0 +1,33 @@
+export interface TeamObject {
+    teamNumber: string,
+    teamName: string,
+    teamOrganization: string,
+}
+
+export class Team {
+    data: {[key: string]: TeamObject | undefined} = {};
+
+    constructor() {}
+
+    add(aTeam: TeamObject) {
+        if (this.data[aTeam.teamNumber] !== undefined) {
+            throw "Division already exist";
+        }
+        if (aTeam.teamNumber.length === 0) {
+            throw "Division name should not be empty";
+        }
+        this.data[aTeam.teamNumber] = aTeam;
+    }
+
+    get() {
+        return this.data;
+    }
+
+    delete(teamNumber: string) {
+        if (this.data[teamNumber] === undefined) {
+            throw "Division does not exist";
+        }
+        this.data[teamNumber] = undefined;
+    }
+
+}
