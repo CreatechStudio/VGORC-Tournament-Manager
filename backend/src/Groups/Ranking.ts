216f890af8ed2656ac3228b89d724b5c53536d43
lightumcc <jessiezhu@createchstudio.com>
2024-10-19 10:11:51 +0800

    style(backend): ranking new line

--- backend/src/Groups/Ranking.ts
+++ backend/src/Groups/Ranking.ts
@@ -4,7 +4,8 @@ import {Ranking} from "../../../common/Ranking";
 export const rankingGroup = new Elysia()
     .decorate('rank', new Ranking())
     .group('/rank', (app) => app
-        .get(':division', ({ rank, params }) => rank.getRanking(params.division), {
+        .get(':division', ({ rank, params }) =>
+            rank.getRanking(params.division), {
             params: t.Object({
                 division: t.String()
             })
