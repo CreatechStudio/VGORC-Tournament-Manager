fadf984d704b774ed408b781123aab8badbaf68b
Iewnfod <shangzhiqiuyue@outlook.com>
2024-11-12 18:23:05 +0800

    feat(frontend): request throttling

--- frontend/src/net.ts
+++ frontend/src/net.ts
@@ -12,6 +12,14 @@ const axiosInstance = axios.default.create({
     timeout: 5000
 });
 
+interface TempData {
+    data: any,
+    startTime: number
+}
+
+const temp: {[Keys: string]: TempData} = {};
+const TEMP_STORE_TIME = 1000;  // ms
+
 function solveErr(e: never) {
     if (e.response.status === 401) {
         toast.error("Unauthorized");
@@ -24,9 +32,51 @@ function solveErr(e: never) {
     }
 }
 
+function getTempKey(url: string, data?: any) {
+    return url + Base64.encode(JSON.stringify(data));
+}
+
+function insertTemp(tempKey: string, data: any) {
+    const current = new Date().getTime();
+    temp[tempKey] = {
+        data: data,
+        startTime: current
+    };
+
+    // 删除超时的数据
+    setTimeout(() => {
+        for (let i = 0; i < temp.length; i++) {
+            if (current - temp[i].startTime > TEMP_STORE_TIME) {
+                temp[i] = undefined;
+            }
+        }
+    }, 0);
+}
+
+function getTempData(tempKey: string) {
+    const current = new Date().getTime();
+
+    const d = temp[tempKey];
+    if (d === undefined) {
+        return undefined;
+    } else {
+        if (current - d.startTime > TEMP_STORE_TIME) {
+            temp[tempKey] = undefined;
+            return undefined;
+        } else {
+            return d.data;
+        }
+    }
+}
+
 export async function getReq(url: string) {
+    const k = getTempKey(url);
+    const d = getTempData(k);
+    if (d !== undefined) return d;
+
     try {
         const res = await axiosInstance.get(`/api/${url}`);
+        insertTemp(k, res.data);
         return res.data;
     } catch (e) {
         solveErr(e);
@@ -35,8 +85,13 @@ export async function getReq(url: string) {
 }
 
 export async function postReq(url: string, data = {}) {
+    const k = getTempKey(url, data);
+    const d = getTempData(k);
+    if (d !== undefined) return d;
+
     try {
         const res = await axiosInstance.post(`/api/${url}`, data);
+        insertTemp(k, res.data);
         return res.data;
     } catch (e) {
         solveErr(e);
@@ -45,8 +100,13 @@ export async function postReq(url: string, data = {}) {
 }
 
 export async function deleteReq(url: string) {
+    const k = getTempKey(url, data);
+    const d = getTempData(k);
+    if (d !== undefined) return d;
+
     try {
         const res = await axiosInstance.delete(`/api/${url}`);
+        insertTemp(k, res.data);
         return res.data;
     } catch (e) {
         solveErr(e);
