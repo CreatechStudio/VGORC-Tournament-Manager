07fa0e2a599deb69a018ea2e06c7b6934d061942
lightumcc <jessiezhu@createchstudio.com>
2024-11-14 10:37:11 +0800

    feat(frontend): vendor logo on printing

--- frontend/src/components/PrintTable.tsx
+++ frontend/src/components/PrintTable.tsx
@@ -1,4 +1,6 @@
+import React, {useRef, useState} from "react";
 import {
+    Box,
     Button,
     DialogContent,
     DialogTitle,
@@ -9,10 +11,9 @@ import {
     Table,
     Typography
 } from "@mui/joy";
-import React, {useRef, useState} from "react";
 import PrintIcon from '@mui/icons-material/Print';
 import {useReactToPrint} from "react-to-print";
-import {PAD, PAD2} from "../constants.ts";
+import {PAD, PAD2, PICTURES} from "../constants.ts";
 
 export default function PrintTable({
     head,
@@ -28,17 +29,27 @@ export default function PrintTable({
         contentRef: tableRef,
     });
     const [open, setOpen] = useState(false);
+    const [selectedImage, setSelectedImage] = useState("");
+    const originalTitle = useRef(document.title);
+
+    const handleOpen = () => {
+        setOpen(true);
+        document.title = title;
+        const randomImage = PICTURES[Math.floor(Math.random() * PICTURES.length)].url;
+        setSelectedImage(randomImage);
+    };
 
     return (
         <React.Fragment>
-            <IconButton onClick={() => {
-                setOpen(true);
-            }}>
+            <IconButton onClick={handleOpen}>
                 <PrintIcon/>
             </IconButton>
             <Modal
                 open={open}
-                onClose={() => setOpen(false)}
+                onClose={() => {
+                    setOpen(false);
+                    document.title = originalTitle.current;
+                }}
             >
                 <ModalDialog layout="fullscreen" sx={{p: PAD2}}>
                     <ModalClose/>
@@ -58,20 +69,25 @@ export default function PrintTable({
                         </Button>
                         <Table
                             ref={tableRef}
-                            borderAxis="both"
                         >
                             <thead>
                             <tr>
-                                <th colSpan={head.length} style={{textAlign: 'center'}}>
-                                    <h2>
-                                        {title}
-                                    </h2>
+                                <th colSpan={head.length} style={{textAlign: 'center', borderRadius: 0}}>
+                                    <Box sx={{
+                                        display: 'flex', alignItems: 'center', justifyContent: 'space-between',
+                                        flexDirection: 'row', p: PAD
+                                    }}>
+                                        <Typography level="title-lg">
+                                            {title}
+                                        </Typography>
+                                        <img src={selectedImage} alt="Logo" style={{height: `${PAD2}vh`}}/>
+                                    </Box>
                                 </th>
                             </tr>
                             <tr>
                                 {
                                     head.map((h, i) => (
-                                        <th key={i}>
+                                        <th key={i} style={{borderRadius: 0}}>
                                             <Typography level="title-md" sx={{textAlign: 'center'}}>
                                                 {h}
                                             </Typography>
@@ -86,7 +102,7 @@ export default function PrintTable({
                                     <tr key={index}>
                                         {
                                             b.map((item, i) => (
-                                                <td key={i}>
+                                                <td key={i} style={{borderRadius: 0}}>
                                                     <Typography level="body-md" sx={{textAlign: 'center'}}>
                                                         {item}
                                                     </Typography>
