0af798215ffd5d986055ff582d1bd8518c3e9678
lightumcc <jessiezhu@createchstudio.com>
2025-06-21 09:14:54 +0800

    fix(backend): utils permission

--- backend/src/Groups/Utils.ts
+++ backend/src/Groups/Utils.ts
@@ -1,9 +1,23 @@
-import {Elysia} from "elysia";
+import {Elysia, status} from "elysia";
 import {Utils} from "../Utils";
+import {checkJWT} from "../Runtime/Auth";
+
+const MODULE_PERMISSION = "admin"
 
 export const utilsGroup = new Elysia()
     .decorate('db', new Utils())
     .group('/utils', (app) => app
         .get('database/isLocked', ({ db }) => db.isDatabaseLocked())
-        .get('database/lock', ({ db }) => db.setDatabaseLock())
+        .guard(
+            {
+                async beforeHandle({ cookie: { permission } }) {
+                    if (permission === undefined) {
+                        return status(401, "Unauthorized");
+                    }
+                    return await checkJWT(permission.value || "", MODULE_PERMISSION, status);
+                }
+            },
+            (app) => app
+                .get('database/lock', ({ db }) => db.setDatabaseLock())
+        )
     );
